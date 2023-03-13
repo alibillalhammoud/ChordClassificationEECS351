a5baa1093dab85c7798034f803ec9930fa8edeab
alibillalhammoud <70279295+alibillalhammoud@users.noreply.github.com>
2023-03-13 18:51:49 -0400

    spectrogram.py bug fixes

--- src/spectrogram.py
+++ src/spectrogram.py
@@ -11,19 +11,26 @@ args = parser.parse_args()
 
 # Load audio file
 sample_rate, samples = wavfile.read(args.src)
+length = samples.shape[0] / sample_rate
+time = np.linspace(0., length, samples.shape[0])
+
+
 
 # Define spectrogram parameters
 nfft = 1 # length of the FFT window
 window = signal.windows.hann(nfft)
 
 # Calculate spectrogram
-frequencies, times, spectrogram = signal.spectrogram(samples, fs=sample_rate, window=window)
+frequencies, times, spectrogram = signal.spectrogram(samples[:,0], sample_rate)
 
+#print(frequencies)
+#print(times)
 # Plot spectrogram
-plt.pcolormesh(times, frequencies, spectrogram, shading='gouraud')
-#plt.xlabel('Time (s)')
-#plt.ylabel('Frequency (Hz)')
-#plt.title('Spectrogram of Example Song')
-#plt.colorbar()
-#plt.savefig('spectrogram.png')
+#plt.pcolormesh(times, np.fft.fftshift(frequencies), np.fft.fftshift(spectrogram, axes=0))
+plt.pcolormesh(times, frequencies, spectrogram)
+plt.xlabel('Time (s)')
+plt.ylabel('Frequency (Hz)')
+plt.title('Spectrogram of Example Song')
+plt.colorbar()
+plt.savefig('spectrogram.png')
 plt.show()
