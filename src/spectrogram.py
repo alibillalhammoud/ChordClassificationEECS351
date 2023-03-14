8281ac30c6336133c70954064b3a5bbe402e0b12
ThomasKennings <kennings@umich.edu>
2023-03-13 21:51:18 -0400

    cleaned spectrogram code, increased frequency resolution

--- src/spectrogram.py
+++ src/spectrogram.py
@@ -4,30 +4,26 @@ from scipy import signal
 from scipy.io import wavfile
 import argparse
 
-# ArgumentParser for input parse
+# input parse
 parser = argparse.ArgumentParser(description='convert mp3 to wav using ffmpeg (linux) and pydub library')
 parser.add_argument('-s','--src', type=str, help='src file (wav format)')
 args = parser.parse_args()
 
-# Load audio file
+# load audio file
+# caution: WAV files can be 1-channel (mono) or 2-channel (stereo)
 sample_rate, samples = wavfile.read(args.src)
-length = samples.shape[0] / sample_rate
-time = np.linspace(0., length, samples.shape[0])
+print("Num Samples:", samples.shape[0])
+print("Sample Rate:", sample_rate)
 
+# do spectrogram
+window_length = 10000
+window = np.hamming(window_length)
+frequencies, times, spectrogram = signal.spectrogram(samples[:,0], sample_rate, window)
 
-
-# Define spectrogram parameters
-nfft = 1 # length of the FFT window
-window = signal.windows.hann(nfft)
-
-# Calculate spectrogram
-frequencies, times, spectrogram = signal.spectrogram(samples[:,0], sample_rate)
-
-#print(frequencies)
-#print(times)
-# Plot spectrogram
-#plt.pcolormesh(times, np.fft.fftshift(frequencies), np.fft.fftshift(spectrogram, axes=0))
+# plot
 plt.pcolormesh(times, frequencies, spectrogram)
+#plt.xlim(0, 60)
+#plt.ylim(0, 1000)
 plt.xlabel('Time (s)')
 plt.ylabel('Frequency (Hz)')
 plt.title('Spectrogram of Example Song')
