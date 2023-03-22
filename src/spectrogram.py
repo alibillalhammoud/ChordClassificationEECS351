a0efd0435abb18634920415ca05d6c0a2af74b6e
ThomasKennings <kennings@umich.edu>
2023-03-21 20:17:57 -0400

    writing report

--- src/spectrogram.py
+++ src/spectrogram.py
@@ -16,8 +16,8 @@ args = parser.parse_args()
 # caution: WAV files can be 1-channel (mono) or 2-channel (stereo)
 sample_rate, samples = wavfile.read(args.src)
 num_samples = samples.shape[0]
-#print("Num Samples:", num_samples)
-#print("Sample Rate:", sample_rate)
+print("Num Samples:", num_samples)
+print("Sample Rate:", sample_rate)
 
 # do spectrogram
 window_length = 10000
@@ -42,23 +42,15 @@ for j in spectrogram_columns:
 for (t, n) in detected_notes_list:
     print("Time: ", t, "Note: ", n)
 
-# plot spectrograms
-#plt.subplot(1, 2, 1)
-plt.pcolormesh(times, frequencies, thresh_spectrogram)
+# plot spectrogram
+plt.pcolormesh(times, frequencies, spectrogram)
 plt.ylim(0, 1000)
-plt.xlim(0, 20)
+plt.xlim(0, 12)
 plt.xlabel('Time (s)')
 plt.ylabel('Frequency (Hz)')
 plt.colorbar()
-
-"""
-plt.subplot(1, 2, 2)
-plt.pcolormesh(times, frequencies, thresh_spectrogram)
-#plt.ylim(0, 1000)
-plt.xlabel('Time (s)')
-plt.ylabel('Frequency (Hz)')
-"""
 plt.show()
+
 # plot frequency spectrum at some time
 '''
 data = spectrogram[:, t2ind(52.3)]
