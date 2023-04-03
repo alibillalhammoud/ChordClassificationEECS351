974617325b59a8c1f2c9948e45f7e943ea8f48c8
alibillalhammoud <70279295+alibillalhammoud@users.noreply.github.com>
2023-04-03 02:19:39 -0400

    additional zero pad style

--- src/spectrogram.py
+++ src/spectrogram.py
@@ -1,29 +1,40 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy import signal
-from scipy.io import wavfile
 import argparse
 import math
+
 from util import *
 from notes import *
 
 # parse input
-parser = argparse.ArgumentParser(description='convert mp3 to wav using ffmpeg (linux) and pydub library')
-parser.add_argument('-s','--src', type=str, help='src file (wav format)')
+parser = argparse.ArgumentParser(description='Perform note detection on input src file')
+parser.add_argument('-s','--src', type=argparse.FileType('r'), help='src file (wav format)', required=True)
 args = parser.parse_args()
 
-# load audio file (caution: WAV files can be 1- or 2-channel)
-sample_rate, samples = wavfile.read(args.src)
-num_samples = samples.shape[0]
+# load audio into a 1D numpy array. Merges audio if more than 1 channel
+sample_rate, samples, num_samples = readWAV(args.src.name)
 print("Num Samples:", num_samples)
 print("Sample Rate:", sample_rate)
 
 # do spectrogram
 window_length_samples = 5000    # window too short -> note getting too 'spread out' in frequency
-zero_pad_samples = 2.0*window_length_samples
-Wtot = window_length_samples + zero_pad_samples
+Wtot = 2 * window_length_samples
 window = np.hamming(window_length_samples)
-frequencies, times, spectrogram = signal.spectrogram(samples[:,0], sample_rate, window, nfft=Wtot)
+padtype="beginend"
+if padtype == "end":
+	frequencies, times, spectrogram = signal.spectrogram(samples, fs=sample_rate, nperseg=window_length_samples, nfft=Wtot, noverlap=0)
+elif padtype== "beginend":
+	window = signal.windows.hann(window_length_samples)
+	pad_width = 100 # begin and end with zeros
+	window = np.pad(window, pad_width=((pad_width, pad_width)), mode='constant')
+	frequencies, times, spectrogram = signal.spectrogram(samples, fs=sample_rate, window=window, nperseg=window_length_samples+2*pad_width, noverlap=0)
+	#frequencies, times, spectrogram = signal.spectrogram(samples, sample_rate, window, nfft=Wtot)
+#elif padtype == "ends":
+#	frequencies, times, spectrogram = signal.spectrogram(samples, sample_rate, window, nfft=Wtot)
+
+#plotSpectrogram(frequencies,times,spectrogram)
+
 spectrogram /= spectrogram.max()
 spect_rows = len(spectrogram)           # rows represent frequencies
 spect_cols = len(spectrogram[0])        # columns represent time-divisions
@@ -69,87 +80,38 @@ for j in range(spect_cols):
                 detected_notes_list.append([times[j], freq2note(frequencies[i],piano_frequencies)])
 #"""
 
-"""
+
 # List detected notes
-for (t, n) in detected_notes_list:
-    print("Time: ", t, "Note: ", n)
-"""
+#for (t, n) in detected_notes_list:
+#    print("Time: ", t, "Note: ", n)
+
 
 #"""
 # plot spectrogram
 plt.pcolormesh(times, frequencies, thresh_spectrogram)
 plt.ylim(0, 1000)
-plt.xlim(0, 14)
 plt.xlabel('Time (s)')
 plt.ylabel('Frequency (Hz)')
 plt.colorbar()
 plt.show()
 #"""
 
-"""
-# plot frequency spectrum at some time
-data = spectrogram[:, t2ind(52.3, times)]
-plt.plot(frequencies, data)
-plt.xlim(0, 1000)
-plt.show()
-"""
 
-"""
-# plot volume over time
-plt.plot(times, volume)
-plt.xlim(0, 15)
-plt.show()
-"""
+## plot frequency spectrum at some time
+#data = spectrogram[:, t2ind(52.3, times)]
+#plt.plot(frequencies, data)
+#plt.xlim(0, 1000)
+#plt.show()
 
-"""
-# plot volume histogram
-plt.hist(volume, bins=500)
-plt.xlim(0, 0.1)
-plt.show()
-"""
+## plot volume over time
+#plt.plot(times, volume)
+#plt.xlim(0, 15)
+#plt.show()
 
-##################### OLD STUFF ##########################
-"""
-# perform basic thresholding (original spectrogram)
-magn_threshold = 0.9
-thresh_spectrogram = 0*spectrogram
-detected_notes_list = []
-for j in range(spect_cols):
-    for i in range(spect_rows):
-        if spectrogram[i, j] > magn_threshold:
-            thresh_spectrogram[i, j] = 1
-            if not [times[j], freq2note(frequencies[i],piano_frequencies)] in detected_notes_list:
-                detected_notes_list.append([times[j], freq2note(frequencies[i],piano_frequencies)])
-"""
+## plot volume histogram
+#plt.hist(volume, bins=500)
+#plt.xlim(0, 0.1)
+#plt.show()
 
-"""
-# interpolate spectrogram in frequency axis
-df = 0.1
-min_f = 0
-max_f = 1000
-fnew_arr = np.arange(min_f, max_f, df)
-interp_spect_rows = len(fnew_arr)
-interp_spectrogram = np.zeros((interp_spect_rows, spect_cols))
-for j in range(spect_cols):
-    interp_spectrogram[:, j] = np.interp(fnew_arr, frequencies, spectrogram[:, j])
+# see git commit history for old stuff
 
-# perform basic thresholding (interpolated spectrogram)
-magn_threshold = 0.4
-thresh_spectrogram = 0*interp_spectrogram
-detected_notes_list = []
-for j in range(spect_cols):
-    for i in range(interp_spect_rows):
-        if interp_spectrogram[i, j] > magn_threshold:
-            thresh_spectrogram[i, j] = 1
-            if not [times[j], freq2note(fnew_arr[i],piano_frequencies)] in detected_notes_list:
-                detected_notes_list.append([times[j], freq2note(fnew_arr[i],piano_frequencies)])
-
-# plot spectrogram
-#plt.pcolormesh(times, fnew_arr, interp_spectrogram)
-plt.ylim(0, 1000)
-#plt.xlim(0, 12)
-plt.xlabel('Time (s)')
-plt.ylabel('Frequency (Hz)')
-plt.colorbar()
-plt.show()
-"""
--- src/util.py
+++ src/util.py
@@ -1,7 +1,27 @@
+import os
+import numpy as np
+from scipy.io import wavfile
+import matplotlib.pyplot as plt
+
+def readWAV(audio_file: str):
+	"""If the audio data has multiple channels combine them by summing"""
+	if not isinstance(audio_file, str):
+		raise TypeError("Argument must be a string")
+	sample_rate, samples = wavfile.read(audio_file)
+	# only take first audio channel and write to a new WAV file
+	if len(samples.shape) > 1:
+		#samples = samples[:,0]
+		samples = samples.sum(axis=1) # we may do something more advanced in the future
+		# samples = samples / np.max(np.abs(samples)) #normalize audio data [-1, 1]
+		merged_wavfile = os.path.dirname(os.path.abspath(audio_file)) + "/merged_" + os.path.basename(audio_file)
+		wavfile.write(merged_wavfile, sample_rate, samples)
+	return sample_rate, samples, len(samples)
+
 
 # takes in a time and returns the corresponding index in the spectrogram matrix
 def t2ind(t, times):
     return next(x for x, val in enumerate(times) if val > t)
+
 
 def freq2note(freq, piano_frequencies):
     smallest_diff = 1e6
@@ -12,3 +32,11 @@ def freq2note(freq, piano_frequencies):
             note = n
     return note
 
+
+def plotSpectrogram(frequencies,times,spectrogram):
+	plt.pcolormesh(times, frequencies, np.log10(spectrogram))
+	plt.ylim(0, 1000)
+	plt.xlabel('Time (s)')
+	plt.ylabel('Frequency (Hz)')
+	plt.colorbar()
+	plt.show()
