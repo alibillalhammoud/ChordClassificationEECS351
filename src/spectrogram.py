c810cf2c8ea0a41c410bd5c504f379f4f569df4c
ThomasKennings <kennings@umich.edu>
2023-04-08 16:51:05 -0400

    added harmonic filtering and started with bass boosting code

--- src/spectrogram.py
+++ src/spectrogram.py
@@ -1,3 +1,4 @@
+from statistics import harmonic_mean
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy import signal
@@ -33,13 +34,18 @@ volume_threshold = 0.01
 col_norm_spectrogram = do_col_normalization(spectrogram, volume, volume_threshold)
 
 # boost magnitude of low-frequency notes
+#bass_boosted_spectrogram = do_bass_boost(col_norm_spectrogram, frequencies, 350, 2)
+#bass_boosted_spectrogram = do_col_normalization(bass_boosted_spectrogram, volume, volume_threshold)
 
-# do harmonic correction
-harmonic_spectrogram = get_harmonic_spectrogram(col_norm_spectrogram, 0.01)
-harmonic_corrected_spectrogram = col_norm_spectrogram * harmonic_spectrogram
+# 'cut' notes which are loud enough and have a signficant note 1 octave below
+# harmonic_threshold: how loud does note and octave need to be before being cut
+# harmonic_cut_amount: decimal representing amount of harmonic note that is removed
+harmonic_cut_amount = 0.5
+harmonic_threshold = 0.1
+harmonic_corrected_spectrogram = do_harmonic_correction(col_norm_spectrogram, harmonic_threshold, harmonic_cut_amount)
 
 # do thresholding and note detection
-magn_threshold = 0.05
+magn_threshold = 0.1
 thresh_spectrogram, detected_notes_list = do_thresholding(harmonic_corrected_spectrogram, times, frequencies, note_frequencies, magn_threshold)
 
 #print_detected_notes(detected_notes_list)
@@ -48,9 +54,9 @@ thresh_spectrogram, detected_notes_list = do_thresholding(harmonic_corrected_spe
 
 #"""
 # plot spectrogram
-plt.pcolormesh(times, frequencies, thresh_spectrogram)
+plt.pcolormesh(times, frequencies, harmonic_corrected_spectrogram)
 plt.ylim(0, 1000)
-plt.xlim(0, 15)
+plt.xlim(3.5, 15)
 plt.xlabel('Time (s)')
 plt.ylabel('Frequency (Hz)')
 plt.colorbar()
